fb7f5b9f3f907e6136290299d44f05d859668a36
Mentor-Ntech <oloyedeaderayo11@gmail.com>
2024-12-07 16:00:00 +0100

    Your commit message here

--- app/Navbar/Navbar.js
+++ app/Navbar/Navbar.js
@@ -18,7 +18,7 @@ const Navbar = () => {
       <Link href="/">
       <div className='flexCenter md:hiden cursor-pointer' onClick={() => {}}>
         <Image src={images.logo02} objectFit='contain' width={32} height={32} alt='logo'/>
-        <p className='dark:text-white text-nft-black-1 font-semibold text-lg ml-1'> CryptoKet </p>
+        <p className='dark:text-white text-nft-black-1 font-semibold text-lg ml-1'>CryptoKet </p>
       </div>
       </Link>
     </div>
